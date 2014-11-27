53a9cf8a940969e88d2618db379e2f92f2107ffb
Florian Keller <mail@floriankeller.de>
2014-11-27 12:25:02 +0100

    Added modal confirmation
    Taken from http://heera.it/bootstrap-3-delete-confirm-dialog

--- js/script.js
+++ js/script.js
@@ -63,10 +63,24 @@ $(window).load(function() {										// warte darauf, dass der Inhalt geladen wu
 		$tr.find('.editable').show();
 	});
 
-	$('.deletelink').click(function() {
-		if (confirm("Eintrag wirklich löschen?")) {
-			$(this).parent().parent().hide();
-		}
+	$('.deletelink').on('click', function(e) {
+		e.preventDefault();
+		var el = $(this).parent();
+		var title = el.attr('data-title');
+		var msg = el.attr('data-message');
+		var dataForm = el.attr('data-form');
+
+		$('#formConfirm')
+			.find('#frm_body').html(msg)
+			.end().find('#frm_title').html(title)
+			.end().modal('show');
+
+		//$('#formConfirm').find('#frm_submit').attr('data-form', dataForm);
+	});
+
+	$('#formConfirm').on('click', '#frm_submit', function(e) {
+		/*var id = $(this).attr('data-form');
+		$(id).submit();*/
 	});
 
 	initialize();
