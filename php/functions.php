c627e7a68f9f815504b5d3d1e5bc0e5e46117841
Florian Keller <mail@floriankeller.de>
2014-11-29 23:46:00 +0100

    Created get_entries() and get_data()

--- php/functions.php
+++ php/functions.php
@@ -1,9 +1,3 @@
-<!doctype html>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	</head>
-	<body>
 <?php
 	/**
 	* functions.php
@@ -18,20 +12,64 @@
 
 	require_once('dbconnect.php');
 
-	$query = "SELECT d.date, d.temp, d.city_id, d.comment
-			  FROM data d";
+	$actions = array('getdata', 'entries');
 
-	if ($result = $mysqli->query($query)) {
-		while ($row = $result->fetch_object()) {
-			echo "Datum: " . $row->date . "<br>\n
-			Temperatur: " . $row->temp . "<br>\n
-			Ort: " . $row->city_id . "<br>\n
-			Kommentar: " . $row->comment . "<br>\n<br>\n";
-		}	
+	$actionGET = (isset($_GET['action']) ? $_GET['action'] : "");
+	$entryGET  = (isset($_GET['entry'])  ? $_GET['entry']  : "");
+
+	if (in_array($actionGET, $actions)) {
+		switch ($actionGET) {
+			case 'getdata':
+				if (is_numeric($entryGET)) {
+					get_data($entryGET);
+				} else {
+					get_data(null);
+				}
+				break;
+			case 'entries':
+				get_entries();
+		}
 	}
 
-	if ($mysqli->error) {
-		printf("Error: %s\n", $mysqli->error);
+	function get_entries() {
+		global $mysqli;
+
+		$query = "SELECT id
+				  FROM data";
+		if ($result = $mysqli->query($query)) {
+			echo (mysqli_num_rows($result));
+		} else {
+			echo "error";
+		}
+	}
+
+	function get_data($entry) {
+		global $mysqli;
+
+		if ($entry) {
+			$query = "SELECT c.id, c.name, d.id, d.date, d.temp, d.city_id, d.image, d.comment
+					  FROM data d
+					  INNER JOIN cities c on (c.id = d.city_id)
+					  WHERE d.id = " . $entry;
+		} else {
+			$query = "SELECT c.id, c.name, d.date, d.temp, d.city_id, d.image, d.comment
+					  FROM data d
+					  INNER JOIN cities c on (c.id = d.city_id)";
+		}
+
+		if ($result = $mysqli->query($query)) {
+			while ($row = $result->fetch_object()) {
+				echo "Datum: " . $row->date . "<br>\n
+				Temperatur: " . $row->temp . "<br>\n
+				Ort: " . $row->name . "<br>\n
+				Bild: <img src=\"../img/data/" . $row->image . "\" /><br>\n
+				Kommentar: " . $row->comment . "<br>\n<br>\n";
+			}	
+		}
+
+		if ($mysqli->error) {
+			printf("Error: %s\n", $mysqli->error);
+		}
 	}
 ?>
 	</body>
